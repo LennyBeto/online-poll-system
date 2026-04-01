f09dd3d06a9f319a6070f72707fe0685484ff32e
Lenny Beto <lennybeto95@gmail.com>
2026-04-01 21:37:18 +0300

    feat: added admin details to polls

--- config/polls/admin.py
+++ config/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Poll, Option, Vote
 
-# Register your models here.
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 2
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['question', 'created_by', 'total_votes', 'status', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['question']
+    inlines = [OptionInline]
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ['poll', 'option', 'ip_address', 'created_at']
+    list_filter = ['created_at']
+    readonly_fields = ['poll', 'option', 'voter', 'ip_address', 'created_at']
